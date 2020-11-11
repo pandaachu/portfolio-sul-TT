6f340a25365237c991acc80df46f709310f5688d
pandaachu <pandaachu@gmail.com>
2020-11-11 13:12:37 +0800

    Update 2020-11-11T05:11:57.465Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,55 +1,48 @@
-var mobile = !/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-  navigator.userAgent
-)
-  ? false
-  : true;
+"use strict";
 
-$(document).ready(function() {
+var mobile = !/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? false : true;
+$(document).ready(function () {
   loading;
   var countImages = $(".time-wrapper img").length;
-  $(".time-wrapper")
-    .imagesLoaded()
-    .done(function(instance) {
-      $("#progress-bar").css({
-        width: "100%"
-      });
-      TweenMax.to("#loading", 0.5, {
-        delay: 2,
-        display: "none",
-        autoAlpha: 0,
-        ease: Quart.easeOut,
-        onComplete: function() {
-          // onComplete do something
-          $(".time-kv ").addClass("on");
-          setTimeout(function() {
-            $(".kvSection .kvTitle ").addClass("on");
-            $(".kvSection .kv-en").addClass("on");
-          }, 500);
-        }
-      });
-    })
-    .progress(function(instance, image) {
-      if (image.isLoaded) {
-        $(image.img).addClass("loaded");
-        var countLoadedImages = $(".time-wrapper img.loaded").length;
-        var width = 100 * (countLoadedImages / countImages) + "%";
-        $("#progress-bar").css({ width: width });
+  $(".time-wrapper").imagesLoaded().done(function (instance) {
+    $("#progress-bar").css({
+      width: "100%"
+    });
+    TweenMax.to("#loading", 0.5, {
+      delay: 2,
+      display: "none",
+      autoAlpha: 0,
+      ease: Quart.easeOut,
+      onComplete: function onComplete() {
+        // onComplete do something
+        $(".time-kv ").addClass("on");
+        setTimeout(function () {
+          $(".kvSection .kvTitle ").addClass("on");
+          $(".kvSection .kv-en").addClass("on");
+        }, 500);
       }
     });
-
-  // ingredient mobile aniamtion
+  }).progress(function (instance, image) {
+    if (image.isLoaded) {
+      $(image.img).addClass("loaded");
+      var countLoadedImages = $(".time-wrapper img.loaded").length;
+      var width = 100 * (countLoadedImages / countImages) + "%";
+      $("#progress-bar").css({
+        width: width
+      });
+    }
+  }); // ingredient mobile aniamtion
 
   if ($(window).width() <= 767) {
-    $(".igdtWrap").click(function() {
+    $(".igdtWrap").click(function () {
       $(this).toggleClass("on");
     });
     $(".honorstige-slider").addClass("owl-carousel");
-  }
-
-  // ingredient mobile aniamtion
-
+  } // ingredient mobile aniamtion
   // mainMenu START
-  $(".navigation_trigger").on("click", function() {
+
+
+  $(".navigation_trigger").on("click", function () {
     $(".navigation_trigger").addClass("nav_in");
     $(".nav_cover").addClass("nav_in");
     $(".contents_nav").addClass("nav_in");
@@ -59,7 +52,7 @@ $(document).ready(function() {
     $("body").addClass("fixed");
     return false;
   });
-  $(".nav_close").on("click", function() {
+  $(".nav_close").on("click", function () {
     $(".navigation_trigger").removeClass("nav_in");
     $(".nav_cover").removeClass("nav_in");
     $(".contents_nav").removeClass("nav_in");
@@ -69,7 +62,7 @@ $(document).ready(function() {
     $("body").removeClass("fixed");
     return false;
   });
-  $(".nav_list a").on("click", function() {
+  $(".nav_list a").on("click", function () {
     $(".navigation_trigger").removeClass("nav_in");
     $(".nav_cover").removeClass("nav_in");
     $(".contents_nav").removeClass("nav_in");
@@ -77,47 +70,37 @@ $(document).ready(function() {
     $(".nav_list").removeClass("nav_in");
     $(".menu").removeClass("nav_in");
     $("body").removeClass("fixed");
-  });
-  // mainMenu END
-
+  }); // mainMenu END
   //slider Start
-  $(".product-owl-carousel")
-    .on("initialized.owl.carousel changed.owl.carousel", function(e) {
-      if (!e.namespace) {
-        return;
-      }
-      var carousel = e.relatedTarget;
-      var cnum = carousel.relative(carousel.current());
-      $(".btns a").removeClass("on");
-      $(".btns a")
-        .eq(cnum)
-        .addClass("on");
-      $(".btns").removeClass("bg00 bg01 bg02 bg03 bg04 bg05 bg06 bg07");
-      $(".btns").addClass("bg0" + cnum);
-    })
-    .owlCarousel({
-      items: 1,
-      loop: true,
-      margin: 0,
-      autoplay: true,
-      slideSpeed: 5000,
-      paginationSpeed: 5000,
-      autoplayHoverPause: true,
-      nav: true,
-      navText: [
-        "<img src='assets/images/leftArrow.png'>",
-        "<img src='assets/images/rightArrow.png'>"
-      ]
-    });
 
+  $(".product-owl-carousel").on("initialized.owl.carousel changed.owl.carousel", function (e) {
+    if (!e.namespace) {
+      return;
+    }
+
+    var carousel = e.relatedTarget;
+    var cnum = carousel.relative(carousel.current());
+    $(".btns a").removeClass("on");
+    $(".btns a").eq(cnum).addClass("on");
+    $(".btns").removeClass("bg00 bg01 bg02 bg03 bg04 bg05 bg06 bg07");
+    $(".btns").addClass("bg0" + cnum);
+  }).owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    slideSpeed: 5000,
+    paginationSpeed: 5000,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: ["<img src='assets/images/leftArrow.png'>", "<img src='assets/images/rightArrow.png'>"]
+  });
   var owldata = $(".owl-carousel").data("owl.carousel");
-  $(".btns a").on("click", function() {
+  $(".btns a").on("click", function () {
     var num = $(this).index();
     owldata.to(num);
     return false;
-  });
-
-  // honorstige - slider
+  }); // honorstige - slider
 
   $(".honorstige-slider.owl-carousel").owlCarousel({
     items: 1,
@@ -128,117 +111,77 @@ $(document).ready(function() {
     paginationSpeed: 5000,
     autoplayHoverPause: true,
     nav: true,
-    navText: [
-      "<img src='assets/images/honorstige-left-arrow.png'>",
-      "<img src='assets/images/honorstige-right-arrow.png'>"
-    ]
-  });
-  // slider END
-
+    navText: ["<img src='assets/images/honorstige-left-arrow.png'>", "<img src='assets/images/honorstige-right-arrow.png'>"]
+  }); // slider END
   //   table START
-  $(".locationSelect select").on("change", function() {
+
+  $(".locationSelect select").on("change", function () {
     $(".locationResult .result").removeClass("on");
+
     if ($(this).val() == "01") {
-      $(".locationResult .result")
-        .eq(0)
-        .addClass("on");
+      $(".locationResult .result").eq(0).addClass("on");
     } else if ($(this).val() == "02") {
-      $(".locationResult .result")
-        .eq(1)
-        .addClass("on");
+      $(".locationResult .result").eq(1).addClass("on");
     } else if ($(this).val() == "03") {
-      $(".locationResult .result")
-        .eq(2)
-        .addClass("on");
+      $(".locationResult .result").eq(2).addClass("on");
     } else if ($(this).val() == "04") {
-      $(".locationResult .result")
-        .eq(3)
-        .addClass("on");
+      $(".locationResult .result").eq(3).addClass("on");
     } else if ($(this).val() == "05") {
-      $(".locationResult .result")
-        .eq(4)
-        .addClass("on");
+      $(".locationResult .result").eq(4).addClass("on");
     } else if ($(this).val() == "06") {
-      $(".locationResult .result")
-        .eq(5)
-        .addClass("on");
+      $(".locationResult .result").eq(5).addClass("on");
     } else if ($(this).val() == "07") {
-      $(".locationResult .result")
-        .eq(6)
-        .addClass("on");
+      $(".locationResult .result").eq(6).addClass("on");
     } else if ($(this).val() == "08") {
-      $(".locationResult .result")
-        .eq(7)
-        .addClass("on");
+      $(".locationResult .result").eq(7).addClass("on");
     } else if ($(this).val() == "09") {
-      $(".locationResult .result")
-        .eq(8)
-        .addClass("on");
+      $(".locationResult .result").eq(8).addClass("on");
     } else if ($(this).val() == "10") {
-      $(".locationResult .result")
-        .eq(9)
-        .addClass("on");
+      $(".locationResult .result").eq(9).addClass("on");
     } else if ($(this).val() == "11") {
-      $(".locationResult .result")
-        .eq(10)
-        .addClass("on");
+      $(".locationResult .result").eq(10).addClass("on");
     } else if ($(this).val() == "12") {
-      $(".locationResult .result")
-        .eq(11)
-        .addClass("on");
+      $(".locationResult .result").eq(11).addClass("on");
     } else if ($(this).val() == "13") {
-      $(".locationResult .result")
-        .eq(12)
-        .addClass("on");
+      $(".locationResult .result").eq(12).addClass("on");
     } else if ($(this).val() == "14") {
-      $(".locationResult .result")
-        .eq(13)
-        .addClass("on");
+      $(".locationResult .result").eq(13).addClass("on");
     } else if ($(this).val() == "15") {
-      $(".locationResult .result")
-        .eq(14)
-        .addClass("on");
+      $(".locationResult .result").eq(14).addClass("on");
     } else if ($(this).val() == "16") {
-      $(".locationResult .result")
-        .eq(15)
-        .addClass("on");
+      $(".locationResult .result").eq(15).addClass("on");
     } else if ($(this).val() == "17") {
-      $(".locationResult .result")
-        .eq(16)
-        .addClass("on");
+      $(".locationResult .result").eq(16).addClass("on");
     }
+
     return false;
   });
-
-  $(".gotopBtn").on("click", function() {
-    $("html, body").animate(
-      {
-        scrollTop: $(".time-kv").offset().top - 100
-      },
-      700
-    );
+  $(".gotopBtn").on("click", function () {
+    $("html, body").animate({
+      scrollTop: $(".time-kv").offset().top - 100
+    }, 700);
     return false;
-  });
-  //   table END
-
+  }); //   table END
   //   animate START
+
   AOS.init({
     duration: 1200,
     useClassNames: true,
     initClassName: false,
     animatedClassName: "animated"
-  });
-  //   animate END
-
+  }); //   animate END
   // pv setting
+
   var _preview = "";
+
   function debounce(func, wait, immediate) {
     var timeout;
-    return function() {
+    return function () {
       clearTimeout(timeout);
       timeout = setTimeout(func, wait);
     };
   }
+
   function Tracking() {
     var range = 50;
     var t1 = $(".intro").offset().top - range;
@@ -249,6 +192,7 @@ $(document).ready(function() {
     var t6 = $(".time-event2-txt").offset().top - range;
 
     var _top = $(window).scrollTop() + $(window).height() / 2;
+
     if (_top >= t1 && _top <= t2) {
       if (_preview != "Intro") {
         _preview = "Intro";
@@ -287,21 +231,20 @@ $(document).ready(function() {
       }
     }
   }
+
   window.addEventListener("scroll", debounce(Tracking, 500));
+
   if (_preview == "") {
     _preview = "Intro";
-  }
+  } //scroll到底觸發追蹤
 
-  //scroll到底觸發追蹤
-  $(window).scroll(function() {
+
+  $(window).scroll(function () {
     // Returns height of browser viewport
     var window_height = $(window).height();
+    var window_scrollTop = $(window).scrollTop(); // Returns height of HTML document
 
-    var window_scrollTop = $(window).scrollTop();
-
-    // Returns height of HTML document
     var document_height = $(document).height();
-
     console.log(window_height);
     console.log(window_scrollTop);
     console.log(document_height);
@@ -310,31 +253,29 @@ $(document).ready(function() {
       TrackEvent("Timetreasure", "Scroll", "Store");
     }
   });
-
-  $(".shop-btn-pc").on("click", function() {
+  $(".shop-btn-pc").on("click", function () {
     TrackEvent("Timetreasure", "Click", "Store_btn");
   });
-  $(".shop-btn-mb").on("click", function() {
+  $(".shop-btn-mb").on("click", function () {
     TrackEvent("Timetreasure", "Click", "Store");
   });
-  $(".shopBtn2").on("click", function() {
+  $(".shopBtn2").on("click", function () {
     TrackEvent("Timetreasure", "Click", "Store_PC");
   });
-  $(".more-btn-cream").on("click", function() {
+  $(".more-btn-cream").on("click", function () {
     TrackEvent("Timetreasure", "Click", "Honorstige_Cream");
   });
-  $(".more-btn-serum").on("click", function() {
+  $(".more-btn-serum").on("click", function () {
     TrackEvent("Timetreasure", "Click", "Honorstige_Serum");
   });
-  
-});
+}); // youtube api
 
-// youtube api
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
+
 function onYouTubePlayerAPIReady() {
   player = new YT.Player("ytplayer", {
     width: "100%",
@@ -352,21 +293,22 @@ function onYouTubePlayerAPIReady() {
     }
   });
 }
+
 function onPlayerReady(event) {}
+
 function onPlayerStateChange(event) {
   // player狀態變更成播完
-  if (event.data == 0) {
-    // 裡面放追蹤碼
+  if (event.data == 0) {// 裡面放追蹤碼
   }
 }
+
 function stopVideo() {
   player.stopVideo();
-}
+} // on screen animation
 
-// on screen animation
 
-$(window).scroll(function() {
-  $(".animate").each(function() {
+$(window).scroll(function () {
+  $(".animate").each(function () {
     if ($(this).isOnScreen(0, 0.1) == true) {
       $(this).addClass("active");
     } else {
@@ -374,3 +316,4 @@ $(window).scroll(function() {
     }
   });
 });
+//# sourceMappingURL=all.js.map
